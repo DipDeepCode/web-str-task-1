297b48ef989ca18f571117275da50b0568495525
svkovalev <svkovalev@bank-hlynov.ru>
2024-05-21 15:31:37 +0300

    WST1 :: init

---
+++ src/main/java/ru/ddc/webstrtask12/WebStrTask12Application.java
@@ -0,0 +1,13 @@
+package ru.ddc.webstrtask12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStrTask12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebStrTask12Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ddc/webstrtask12/WebStrTask12ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ddc.webstrtask12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebStrTask12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
